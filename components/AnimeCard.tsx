38c7942aba845d24265b738024420f4249861641
pandawaa <pallakangoding@gmail.com>
2024-01-03 11:44:35 +0800

    anime-list

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,28 +1,45 @@
-import Image from "next/image";
+import Image from "next/image"
+import { MotionDiv } from "./MotionDiv"
 
 export interface AnimeProp {
-  id: string;
-  name: string;
+  id: string
+  name: string
   image: {
-    original: string;
-  };
-  kind: string;
-  episodes: number;
-  episodes_aired: number;
-  score: string;
+    original: string
+  }
+  kind: string
+  episodes: number
+  episodes_aired: number
+  score: string
 }
 
 interface Prop {
-  anime: AnimeProp;
-  index: number;
+  anime: AnimeProp
+  index: number
 }
 
-function AnimeCard({ anime }: Prop) {
+const vairants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+}
+
+function AnimeCard({ anime, index }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      variants={vairants}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        delay: index * 0.25,
+        ease: "easeInOut",
+        duration: 0.5,
+      }}
+      viewport={{ amount: 0 }}
+      className="max-w-sm rounded relative w-full"
+    >
       <div className="relative w-full h-[37vh]">
         <Image
-          src={anime.image.original}
+          src={`https://shikimori.one${anime.image.original}`}
           alt={anime.name}
           fill
           className="rounded-xl"
@@ -64,8 +81,8 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
-  );
+    </MotionDiv>
+  )
 }
 
-export default AnimeCard;
+export default AnimeCard
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image from "next/image"
 
 function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
+      <p className="text-base font-bold text-white">@2024 AnimeJumbo</p>
       <Image
         src="./logo.svg"
         alt="logo"
@@ -35,7 +35,7 @@ function Footer() {
         />
       </div>
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
